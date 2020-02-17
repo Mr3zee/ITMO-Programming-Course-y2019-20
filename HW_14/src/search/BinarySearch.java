227774fd17b3ee6f9dfae47156af84ba8a25a699
Mr3zee <284627@niuitmo.ru>
2020-02-17 23:29:27 +0300

    BinarySearch - Hoare triples added

--- HW_14/src/search/BinarySearch.java
+++ HW_14/src/search/BinarySearch.java
@@ -1,15 +1,24 @@
 package search;
 
 public class BinarySearch {
-    // Pre: args[0] = value, arr[1] >= arr[2] >= ... >= arr[n - 1]
+    // A = (for each -1 <= p < q <= n : arr[p] >= arr[q])
+    // C = for each 0 <= j < n : arr[j]' == arr[j]
+    // Pre : A
     public static void main(String[] args) {
         int value = Integer.parseInt(args[0]);
+        // value = Integer.parseInt(args[0])
+        // C -> A
         int[] arr = new int[args.length - 1];
         for (int i = 1; i < args.length; i++) {
             arr[i - 1] = Integer.parseInt(args[i]);
         }
+        // arr = ((int) args) \ args[0]
+        // C -> A
         SearchEngine engine = new SearchEngine();
-        System.out.println(engine.searchRecur(value, arr));
+        // c -> A
+        int i = engine.searchRecur(value, arr);
+        // arr[i] <= value && (i -> min) && C
+        System.out.println(i);
     }
-    // Post:
+    // Post: arr[i] <= value && (i -> min) && C
 }
