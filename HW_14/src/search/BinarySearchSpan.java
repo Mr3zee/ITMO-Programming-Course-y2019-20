ad9bad26fc87eb622b5590d59fe39e09279559ab
Mr3zee <284627@niuitmo.ru>
2020-02-24 19:35:51 +0300

    Comments modified

--- HW_14/src/search/BinarySearchSpan.java
+++ HW_14/src/search/BinarySearchSpan.java
@@ -1,9 +1,13 @@
 package search;
 
 public class BinarySearchSpan {
-    // A = (for each -1 <= p < q <= arr.length : arr[p] >= arr[q])
-    // C = for each 0 <= j < n : arr[j]' == arr[j]
-    // Pre : A
+    // args.length = n + 1
+    // value = args[0]
+    // arr[0; n - 1] = args[1; n]
+    // A = (for each 0 <= p < q < n : arr[p] >= arr[q])
+    // C = (for each 0 <= j < n : arr[j]' == arr[j])
+    //
+    // Pre : A && n >= 0
     public static void main(String[] args) {
         int span;
         int value = Integer.parseInt(args[0]);
@@ -18,25 +22,12 @@ public class BinarySearchSpan {
         // A
         int i = engine.searchIter(value, arr);
         // arr[i] <= value && (i -> min) && C
-        // First = (arr[i] <= value && (i -> min))
-        if (value == Integer.MIN_VALUE) {
-            // value = Integer.MIN_VALUE -> for each i <= k < arr.length : arr[k] = value -> span = arr.length - i -> arr.length = i + span
-            span = arr.length - i;
-            // (arr[i] <= value && (i -> min)) && (for each i <= k < i + span : arr[k] = arr[i]) && C
-        } else {
-            // value != Integer.MIN_VALUE && C && First
-            int next = value - 1;
-            // value != Integer.MIN_VALUE -> next < value
-            // next < value && C && First
-            // C -> A
-            // A && next < value && First
-            int j = engine.searchIter(next, arr);
-            // arr[j] <= next && (j -> min) && next < value && C && First
-            // (arr[j] <= next && next < value) -> (arr[j] < value && (j -> min))
-            // (arr[j] < value && (j -> min) && First && A) -> for each i <= k < j : arr[k] = arr[i] -> span = j - i -> j = i + span
-            span = j - i;
-            // (arr[i] <= value && (i -> min)) && (for each i <= k < i + span : arr[k] = arr[i]) && C
-        }
+        // C -> A
+        // A
+        int j = engine.searchRecur(value, arr);
+        // arr[j] < value && (j -> min) && C
+        // (arr[i] <= value && (i -> min)) && (arr[j] < value && (j -> min)) -> for each i <= k < j : arr[k] = arr[i] -> span = j - i -> j = i + span
+        span = j - i;
         // (arr[i] <= value && (i -> min)) && (for each i <= k < i + span : arr[k] = arr[i]) && C
         System.out.println(i + " " + span);
     }
