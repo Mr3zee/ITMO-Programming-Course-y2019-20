0351b666995cb134d2c050ce87880f638cecf9d6
Mr3zee <284627@niuitmo.ru>
2020-02-28 02:43:53 +0300

    added SuiteTestClass

---
+++ HW_15/src/SuiteTestClass.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ArrayQueueADTTests.class, ArrayQueueModuleTests.class, ArrayQueueTests.class})
+public class SuiteTestClass {
+
+}
+
