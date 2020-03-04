0a0e308fadb63b0f84207f4d7798209a5541a912
Mr3zee <284627@niuitmo.ru>
2020-03-04 21:04:50 +0300

    DoubleEType

---
+++ HW_17/src/expression/type/DoubleEType.java
@@ -0,0 +1,37 @@
+package expression.type;
+
+public class DoubleEType extends AbstractEType<Double> {
+    public DoubleEType(Double value) {
+        super(value);
+    }
+
+    @Override
+    protected Double calcAdd(Double v) {
+        return value() + v;
+    }
+
+    @Override
+    protected Double calcSubtract(Double v) {
+        return value() - v;
+    }
+
+    @Override
+    protected Double calcMultiply(Double v) {
+        return value() * v;
+    }
+
+    @Override
+    protected Double calcDivide(Double v) {
+        return value() / v;
+    }
+
+    @Override
+    protected Double calcNegate() {
+        return -value();
+    }
+
+    @Override
+    public EType<Double> valueOf(Double v) {
+        return new DoubleEType(v);
+    }
+}
