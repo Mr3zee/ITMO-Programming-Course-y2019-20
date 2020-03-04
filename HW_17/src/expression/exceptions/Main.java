190b0b259996cace6cb119d17fe6cadabda135b0
Mr3zee <284627@niuitmo.ru>
2020-03-05 00:38:38 +0300

    Generics added v1.6

--- HW_17/src/expression/exceptions/Main.java
+++ HW_17/src/expression/exceptions/Main.java
@@ -1,10 +1,16 @@
 package expression.exceptions;
 
 import expression.*;
-import expression.exceptions.EExceptions.*;
+import expression.type.BigIntegerEType;
+import expression.type.IntegerEType;
+
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        String expression = "x y";
+        String expression = "234";
+        CommonExpression<Integer> expression1 = new Const<>(new IntegerEType(Integer.parseInt(expression)));
+        CommonExpression<BigInteger> expression2 = new Const<>(new BigIntegerEType(new BigInteger(expression)));
+        System.out.println(expression1.equals(expression2));
     }
 }
