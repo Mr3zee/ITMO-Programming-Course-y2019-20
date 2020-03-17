f0a1b9c1904301834fddfd4017bf89ff08010c92
Mr3zee <284627@niuitmo.ru>
2020-03-17 12:47:48 +0300

    Generics (map added)

--- HW_17/src/expression/generic/GenericTabulator.java
+++ HW_17/src/expression/generic/GenericTabulator.java
@@ -5,43 +5,38 @@ import expression.exceptions.*;
 import expression.parser.ExpressionParser;
 import expression.type.*;
 
-import java.math.BigInteger;
+import java.util.Map;
 import java.util.function.Function;
 
 public class GenericTabulator implements Tabulator {
-
-    private String expression;
-    private int x1;
-    private int dx;
-    private int y1;
-    private int dy;
-    private int z1;
-    private int dz;
+    private final Map<String, TableGenerator<? extends Number>> generators =
+            Map.of("i", new TableGenerator<>(IntegerEType::parseInteger),
+                    "d", new TableGenerator<>(DoubleEType::parseDouble),
+                    "bi", new TableGenerator<>(BigIntegerEType::parseBigInteger),
+                    "l", new TableGenerator<>(LongEType::parseLong),
+                    "s", new TableGenerator<>(ShortEType::parseShort),
+                    "u", new TableGenerator<>(UncheckedIntegerEType::parseUncheckedInteger));
 
     @Override
     public Object[][][] tabulate(String mode, String expression, int x1, int x2, int y1, int y2, int z1, int z2) throws ExpressionException {
-        initParameters(expression, x1, x2, y1, y2, z1, z2);
-        switch (mode) {
-            case "i":
-                return new TableGenerator<Integer>().generateTable(IntegerEType::parseInteger);
-            case "d":
-                return new TableGenerator<Double>().generateTable(DoubleEType::parseDouble);
-            case "bi":
-                return new TableGenerator<BigInteger>().generateTable(BigIntegerEType::parseBigInteger);
-            case "l":
-                return new TableGenerator<Long>().generateTable(LongEType::parseLong);
-            case "s":
-                return new TableGenerator<Short>().generateTable(ShortEType::parseShort);
-            case "u":
-                return new TableGenerator<Integer>().generateTable(UncheckedIntegerEType::parseUncheckedInteger);
-            default:
-                throw new UnsupportedETypeException(mode);
+        TableGenerator<? extends Number> table = generators.get(mode);
+        if (table == null) {
+            throw new UnsupportedETypeException(mode);
         }
+        return table.generateTable(expression, x1, x2, y1, y2, z1, z2);
     }
 
-    private class TableGenerator<T extends Number> {
+    private static class TableGenerator<T extends Number> {
+        private final Function<String, EType<T>> parseEType;
 
-        private Object[][][] generateTable(Function<String, EType<T>> parseEType) throws ExpressionException {
+        public TableGenerator(Function<String, EType<T>> parseEType) {
+            this.parseEType = parseEType;
+        }
+
+        private Object[][][] generateTable(String expression, int x1, int x2, int y1, int y2, int z1, int z2) throws ExpressionException {
+            int dx = x2 - x1 + 1;
+            int dy = y2 - y1 + 1;
+            int dz = z2 - z1 + 1;
             Object[][][] result = new Object[dx][dy][dz];
             CommonExpression<T> parsedExpression = new ExpressionParser<>(parseEType).parse(expression);
             for (int i = 0; i < dx; i++) {
@@ -60,15 +55,5 @@ public class GenericTabulator implements Tabulator {
             return result;
         }
 
-    }
-
-    private void initParameters(String expression, int x1, int x2, int y1, int y2, int z1, int z2) {
-        this.expression = expression;
-        this.x1 = x1;
-        this.dx = x2 - x1 + 1;
-        this.y1 = y1;
-        this.dy = y2 - y1 + 1;
-        this.z1 = z1;
-        this.dz = z2 - z1 + 1;
     }
 }
