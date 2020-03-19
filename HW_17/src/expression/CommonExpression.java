bfdae8ddf4a28c7ebb52f17dd5df3565fc1a8931
Mr3zee <284627@niuitmo.ru>
2020-03-19 21:58:52 +0300

    minor

--- HW_17/src/expression/CommonExpression.java
+++ HW_17/src/expression/CommonExpression.java
@@ -1,6 +1,6 @@
 package expression;
 
-public interface CommonExpression<T extends Number> extends Expression<T>, ExpressionTriple<T> {
+public interface CommonExpression<T extends Number> extends ExpressionMy<T>, ExpressionTriple<T> {
     // Primary numbers used for hashCode():
     // Binary ops: 3527
     // Add: 3557
--- HW_17/src/expression/ExpressionMy.java
+++ HW_17/src/expression/ExpressionMy.java
@@ -5,6 +5,6 @@ import expression.type.EType;
 /**
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
-public interface Expression<T extends Number> extends ToMiniString {
+public interface ExpressionMy<T extends Number> extends ToMiniString {
     EType<T> evaluate(EType<T> x);
 }
