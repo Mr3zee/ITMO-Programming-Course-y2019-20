70d5021fb7038c172f65769660ea9839be29a5a0
Mr3zee <284627@niuitmo.ru>
2020-03-20 11:00:17 +0300

    added avg5 and med3

--- JS/HW_1/functionalExpression.js
+++ JS/HW_1/functionalExpression.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// let println = function () {
-//     for (let value of arguments) {
-//         console.log(value);
-//     }
-// }
 
 const binaryOperation = operation => (first, second) => (x, y, z) => operation(first(x, y, z), second(x, y, z));
 const add = binaryOperation((a, b) => a + b);
@@ -19,6 +14,29 @@ const cos = unaryOperation(a => Math.cos(a));
 const cube = unaryOperation(a => a * a * a);
 const cuberoot = unaryOperation(a => Math.cbrt(a));
 
+const averageOperation = operation => arity => (...args) => (x, y, z) => {
+    let values = typeof (args[0]) === "object" ? args[0] : args;
+    return operation(arity)(values)(x, y, z);
+};
+
+const avg = averageOperation(arity => (args) => (x, y, z) => {
+    let ans = 0;
+    for (let i = 0; i < arity; i++) {
+        ans += args[i](x, y, z);
+    }
+    return ans / arity;
+});
+const avg5 = avg(5);
+const med = averageOperation(arity => args => (x, y, z) => {
+    let a = [];
+    for (let i = 0; i < arity; i++) {
+        a[i] = args[i](x, y, z);
+    }
+    a.sort((a, b) => a - b);
+    return a[Math.floor(a.length / 2)];
+});
+const med3 = med(3);
+
 const variable = name => (x, y, z) => name === "x" ? x : name === "y" ? y : z;
 const cnst = val => (x, y, z) => val;
 
@@ -30,9 +48,9 @@ const z = variable("z");
 
 const binary = {
     "+" : add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
 };
 
 const unary = {
@@ -51,6 +69,11 @@ const variablesAndConsts = {
     "pi" : pi
 };
 
+const average = {
+    "avg5" : [avg5, 5],
+    "med3" : [med3, 3]
+};
+
 const parseLex = lex => lex in variablesAndConsts ? variablesAndConsts[lex] : cnst(parseInt(lex));
 
 const parse = expression => {
@@ -61,11 +84,19 @@ const parse = expression => {
             stack.push(binary[lex](stack.pop(), second));
         } else if (lex in unary) {
             stack.push(unary[lex](stack.pop()));
+        } else if (lex in average) {
+            let operation = average[lex];
+            stack.push(operation[0](stack.splice(stack.length - operation[1], operation[1])));
         } else {
             stack.push(parseLex(lex));
         }
     });
     return stack.pop();
-}
+};
 
-// println(parse(' 10         ')(0, 0, 0));
+// let println = function () {
+//     for (let value of arguments) {
+//         console.log(value);
+//     }
+// };
+
