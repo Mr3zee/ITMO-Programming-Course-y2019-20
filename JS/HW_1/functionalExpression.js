df74de5e135b15919fb71c6336425d7559ccaca7
Mr3zee <284627@niuitmo.ru>
2020-03-25 01:29:21 +0300

    functionalExpression v 3.1 (reduce, arity, etc)

--- JS/HW_1/functionalExpression.js
+++ JS/HW_1/functionalExpression.js
@@ -1,23 +1,27 @@
 "use strict";
 
-const standardFunction = operation => (...args) => (...vars) => operation(...(args.map(a => a(...vars))));
+const standardFunction = (operation, arity) => {
+    const func = (...args) => (...vars) => operation(...(args.map(a => a(...vars))));
+    func.arity = arity;
+    return func;
+}
 
-const add = standardFunction((a, b) => a + b);
-const subtract = standardFunction((a, b) => a - b);
-const multiply = standardFunction((a, b) => a * b);
-const divide = standardFunction((a, b) => a / b);
-const negate = standardFunction(a => -a);
-const sin = standardFunction(Math.sin);
-const cos = standardFunction(Math.cos);
-const cube = standardFunction(a => a * a * a);
-const cuberoot = standardFunction(Math.cbrt);
+const add = standardFunction((a, b) => a + b, 2);
+const subtract = standardFunction((a, b) => a - b, 2);
+const multiply = standardFunction((a, b) => a * b, 2);
+const divide = standardFunction((a, b) => a / b, 2);
+const negate = standardFunction(a => -a, 1);
+const sin = standardFunction(Math.sin, 1);
+const cos = standardFunction(Math.cos, 1);
+const cube = standardFunction(a => a * a * a, 1);
+const cuberoot = standardFunction(Math.cbrt, 1);
 
-const avg = arity => standardFunction((...args) => args.splice(0, arity).reduce((a, b) => a + b, 0) / arity);
+const avg = arity => standardFunction((...args) => args.splice(0, arity).reduce((a, b) => a + b, 0) / arity, arity);
 const med = arity => standardFunction((...args) => {
     args = args.splice(0, arity);
     args.sort((a, b) => a - b);
     return args[Math.floor(args.length / 2)];
-});
+}, arity);
 
 const avg5 = avg(5);
 const med3 = med(3);
@@ -29,7 +33,7 @@ const vars = {
     "z" : 2
 };
 
-const cnst = val => (...args) => val;
+const cnst = val => () => val;
 
 const pi = cnst(Math.PI);
 const e = cnst(Math.E);
@@ -37,21 +41,18 @@ const x = variable("x");
 const y = variable("y");
 const z = variable("z");
 
-const operations = {
-    "+" : [add, 2],
-    "-" : [subtract, 2],
-    "*" : [multiply, 2],
-    "/" : [divide, 2],
-    "sin" : [sin, 1],
-    "cos" : [cos, 1],
-    "negate" : [negate, 1],
-    "cube" : [cube, 1],
-    "cuberoot" : [cuberoot, 1],
-    "avg5" : [avg5, 5],
-    "med3" : [med3, 3]
-};
-
-const variablesAndConsts = {
+const lexemes = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+    "sin" : sin,
+    "cos" : cos,
+    "negate" : negate,
+    "cube" : cube,
+    "cuberoot" : cuberoot,
+    "avg5" : avg5,
+    "med3" : med3,
     "x" : x,
     "y" : y,
     "z" : z,
@@ -59,29 +60,22 @@ const variablesAndConsts = {
     "pi" : pi
 };
 
-const foldParse = f => regExp => expression => {
-    let stack = [];
-    expression.trim().split(regExp).forEach(f(stack));
-    return stack.pop();
-};
+const foldParse = f => regExp => expression => expression.trim().split(regExp).reduce((a, b) => f(a, b), []).pop();
 
-const parseLex = lex => variablesAndConsts[lex] || cnst(+lex);
-
-const postFixParse = foldParse(stack => lex => {
-    if (lex in operations) {
-        let currOp = operations[lex];
-        stack.push(currOp[0](...stack.splice(stack.length - currOp[1], currOp[1])));
-    } else {
-        stack.push(parseLex(lex));
-    }
+const postFixParse = foldParse((stack, lex) => {
+    let a = lexemes[lex] || cnst(+lex);
+    stack.push(a.arity === undefined ? a : a(...stack.splice(-a.arity)));
+    return stack;
 });
 
 const parse = expression => postFixParse(/\s+/)(expression);
 
-// let println = function () {
-//     for (let value of arguments) {
-//         console.log(value);
-//     }
-// };
-//
-// println(parse("1 x +")(1));
+let println = function () {
+    for (let value of arguments) {
+        console.log(value);
+    }
+};
+
+println(parse("1 x +")(1));
+
+// println(cnst(2).arity);
