449abc62cccc1bdf7b87665c216bb6f34716d106
Mr3zee <284627@niuitmo.ru>
2020-03-27 00:48:40 +0300

    objectExpression v 0.4 (pow log)

--- JS/HW_2/objectExpression.js
+++ JS/HW_2/objectExpression.js
@@ -32,6 +32,7 @@ const vars = {
 const X = new Variable("x");
 const Y = new Variable("y");
 const Z = new Variable("z");
+const e = new Const(Math.E);
 
 function StandardFunction(operation, arity, operand, diff) {
     const f = function (...args) {
@@ -49,17 +50,35 @@ function StandardFunction(operation, arity, operand, diff) {
     return f;
 }
 
-const diffAddSub = function(args, differential, Constructor) {
-    args = args.reduce((a, b) => {a.push(b.diff(differential)); return a}, []);
+const diffAddSub = function (args, differential, Constructor) {
+    args = args.reduce((a, b) => {
+        a.push(b.diff(differential));
+        return a
+    }, []);
     return new Constructor(...args);
 }
 
-const diffMultiplyDivide = function(resultFunction) {
+const diffMultiplyDivide = function (resultFunction) {
     return (args, differential, Constructor) => {
-        const f = args.length === 2 ? args.shift() : new Constructor(...args.slice(-1)), g = args.pop();
-        const fg = new Multiply(f.diff(differential), g), gf = new Multiply(g.diff(differential), f);
+        const f = args.length === 2 ? args[0] : new Constructor(...args.slice(-1));
+        const g = args[args.length - 1];
+        const fg = new Multiply(f.diff(differential), g);
+        const gf = new Multiply(g.diff(differential), f);
         return resultFunction(fg, gf, f, g);
     }
+}
+
+const diffPower = function (args, differential) {
+    const f = args.length === 2 ? args[1] : new Power(...args.slice(1));
+    const g = new Log(e, args[0]);
+    const fg = new Multiply(f, g);
+    const gf = new Power(args[0], f);
+    return new Multiply(gf, fg.diff(differential));
+}
+
+const diffLog = function (args, differential) {
+    const f = new Divide(...args.map(a => new Ln(a)).reverse());
+    return f.diff(differential);
 }
 
 const diffMultiply = diffMultiplyDivide((fg, gf) => new Add(fg, gf));
@@ -70,16 +89,47 @@ const Subtract = StandardFunction((a, b) => a - b, 2, '-', diffAddSub);
 const Multiply = StandardFunction((a, b) => a * b, 2, '*', diffMultiply);
 const Divide = StandardFunction((a, b) => a / b, 2, '/', diffDivide);
 const Negate = StandardFunction(a => -a, 1, 'negate', diffAddSub);
+const Power = StandardFunction(Math.pow, 2, 'pow', diffPower);
+const Log = StandardFunction((a, b) => Math.log(Math.abs(b)) / Math.log(Math.abs(a)), 2, 'log', diffLog);
+const Sinh = StandardFunction(Math.sinh, 1, 'sinh');
+const Cosh = StandardFunction(Math.cosh, 1, 'cosh');
+
+const diffEFunctions = (First, cnst) => {
+    return (arg, differential) => {
+        const f = new First(cnst, arg[0]);
+        const g = arg[0].diff(differential);
+        return new Multiply(f, g);
+    }
+}
+
+const diffLn = diffEFunctions(Divide.constructor, new Const(1));
+const diffExp = diffEFunctions(Power.constructor, e);
+
+const Ln = StandardFunction(a => Math.log(Math.abs(a)), 1, 'ln', diffLn);
+const Exponent = StandardFunction(a => Math.pow(Math.E, a), 1, 'exp', diffExp);
+
+const MultiFunction = (f, zero, diff) => (arity, string) =>
+    StandardFunction((...args) => args.splice(0, arity).reduce((a, b) => f(a, b), zero), arity, string, diff);
+const Min = MultiFunction(Math.min, Infinity, diffAddSub);
+const Max = MultiFunction(Math.max, -Infinity, diffAddSub);
+const Min3 = Min(3, "min3");
+const Max5 = Max(5, "max5");
 
 const lexemes = {
-    "+" : Add,
-    "-" : Subtract,
-    "*" : Multiply,
-    "/" : Divide,
-    "negate" : Negate,
-    "x" : X,
-    "y" : Y,
-    "z" : Z,
+    "+": Add,
+    "-": Subtract,
+    "*": Multiply,
+    "/": Divide,
+    "negate": Negate,
+    "min3": Min3,
+    "max5": Max5,
+    "pow": Power,
+    "log": Log,
+    "cosh": Cosh,
+    "sinh": Sinh,
+    "x": X,
+    "y": Y,
+    "z": Z,
 };
 
 const foldParse = f => regExp => expression => expression.trim().split(regExp).reduce((a, b) => f(a, b), []).pop();
@@ -92,14 +142,19 @@ const postFixParse = foldParse((stack, arg) => {
 
 const parse = expression => postFixParse(/\s+/)(expression);
 
-let println = function () {
-    for (let value of arguments) {
-        console.log(value);
-    }
-};
-
-let a = new Divide(new Const(2), X);
-a = a.diff("x");
-println(a.evaluate(2));
-
+// let println = function () {
+//     for (let value of arguments) {
+//         console.log(value);
+//     }
+// };
+//
+// let b = new Subtract(new Variable('x'), new Variable('z'));
+// let c = new Multiply(new Const(4), b);
+// let f = new Add(new Const(2), c);
+// let c1 = new Multiply(new Const(2), b);
+// let f1 = new Add(new Const(1), c1);
+// let a = new Log(f, f1)
+// a = a.diff("x");
+// let h = a.evaluate(2, 3, 3);
+// println(h);
 
