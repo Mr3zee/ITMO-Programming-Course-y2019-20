200a112cba3b2d2b55084efe8e9ce727317880e4
Mr3zee <284627@niuitmo.ru>
2020-03-27 02:28:42 +0300

    objectExpression v 1.0 (pow log sinh cosh)

--- JS/HW_2/objectExpression.js
+++ JS/HW_2/objectExpression.js
@@ -68,6 +68,9 @@ const diffMultiplyDivide = function (resultFunction) {
     }
 }
 
+const diffMultiply = diffMultiplyDivide((fg, gf) => new Add(fg, gf));
+const diffDivide = diffMultiplyDivide((fg, gf, f, g) => new Divide(new Subtract(fg, gf), new Multiply(g, g)));
+
 const diffPower = function (args, differential) {
     const f = args.length === 2 ? args[1] : new Power(...args.slice(1));
     const g = new Log(e, args[0]);
@@ -81,9 +84,6 @@ const diffLog = function (args, differential) {
     return f.diff(differential);
 }
 
-const diffMultiply = diffMultiplyDivide((fg, gf) => new Add(fg, gf));
-const diffDivide = diffMultiplyDivide((fg, gf, f, g) => new Divide(new Subtract(fg, gf), new Multiply(g, g)));
-
 const Add = StandardFunction((a, b) => a + b, 2, '+', diffAddSub);
 const Subtract = StandardFunction((a, b) => a - b, 2, '-', diffAddSub);
 const Multiply = StandardFunction((a, b) => a * b, 2, '*', diffMultiply);
@@ -91,29 +91,24 @@ const Divide = StandardFunction((a, b) => a / b, 2, '/', diffDivide);
 const Negate = StandardFunction(a => -a, 1, 'negate', diffAddSub);
 const Power = StandardFunction(Math.pow, 2, 'pow', diffPower);
 const Log = StandardFunction((a, b) => Math.log(Math.abs(b)) / Math.log(Math.abs(a)), 2, 'log', diffLog);
-const Sinh = StandardFunction(Math.sinh, 1, 'sinh');
-const Cosh = StandardFunction(Math.cosh, 1, 'cosh');
 
-const diffEFunctions = (First, cnst) => {
+const diffUnaryFunctions = function (makeFirst) {
     return (arg, differential) => {
-        const f = new First(cnst, arg[0]);
+        const f = makeFirst(arg);
         const g = arg[0].diff(differential);
         return new Multiply(f, g);
     }
 }
 
-const diffLn = diffEFunctions(Divide.constructor, new Const(1));
-const diffExp = diffEFunctions(Power.constructor, e);
+const diffLn = diffUnaryFunctions((arg) => new Divide(new Const(1), arg[0]));
+const diffExp = diffUnaryFunctions((arg) => new Power(e, arg[0]));
+const diffCosh = diffUnaryFunctions((arg) => new Sinh(...arg));
+const diffSinh = diffUnaryFunctions((arg) => new Cosh(...arg));
 
 const Ln = StandardFunction(a => Math.log(Math.abs(a)), 1, 'ln', diffLn);
 const Exponent = StandardFunction(a => Math.pow(Math.E, a), 1, 'exp', diffExp);
-
-const MultiFunction = (f, zero, diff) => (arity, string) =>
-    StandardFunction((...args) => args.splice(0, arity).reduce((a, b) => f(a, b), zero), arity, string, diff);
-const Min = MultiFunction(Math.min, Infinity, diffAddSub);
-const Max = MultiFunction(Math.max, -Infinity, diffAddSub);
-const Min3 = Min(3, "min3");
-const Max5 = Max(5, "max5");
+const Sinh = StandardFunction(Math.sinh, 1, 'sinh', diffSinh);
+const Cosh = StandardFunction(Math.cosh, 1, 'cosh', diffCosh);
 
 const lexemes = {
     "+": Add,
@@ -121,8 +116,6 @@ const lexemes = {
     "*": Multiply,
     "/": Divide,
     "negate": Negate,
-    "min3": Min3,
-    "max5": Max5,
     "pow": Power,
     "log": Log,
     "cosh": Cosh,
@@ -147,14 +140,4 @@ const parse = expression => postFixParse(/\s+/)(expression);
 //         console.log(value);
 //     }
 // };
-//
-// let b = new Subtract(new Variable('x'), new Variable('z'));
-// let c = new Multiply(new Const(4), b);
-// let f = new Add(new Const(2), c);
-// let c1 = new Multiply(new Const(2), b);
-// let f1 = new Add(new Const(1), c1);
-// let a = new Log(f, f1)
-// a = a.diff("x");
-// let h = a.evaluate(2, 3, 3);
-// println(h);
 
