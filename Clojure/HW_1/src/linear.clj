cd766d06a1777cc1fb6bf8c3d7b4c7ecd29b98b2
Mr3zee <284627@niuitmo.ru>
2020-04-24 04:23:40 +0300

    matrix/cuboid/tensor(w/out pre)

--- Clojure/HW_1/src/linear.clj
+++ Clojure/HW_1/src/linear.clj
@@ -1,8 +1,40 @@
-(defn doV [f] #(apply mapv f %&))
-(def v+ (doV +))
-(def v- (doV -))
-(def v* (doV *))
+(defn sameLength [& args] (every? #(= (count (first args)) (count %1)) args))
+(defn allVectors [& args] (every? vector? args))
+(defn doOp [f]
+  (fn [& args]
+    {:pre [(apply allVectors args) (apply sameLength args)]
+     :post [(vector? %) (= (count (first args)) (count %))]}
+    (apply mapv f args)))
+; vector
+(def v+ (doOp +))
+(def v- (doOp -))
+(def v* (doOp *))
+(def vd (doOp /))
 (defn scalar [& args] (apply + (apply v* args)))
 (defn v*s [vec & args] (mapv #(* (apply * args) %1) vec))
-(defn det2 [a b i1 i2] (- (* (nth a i1) (nth b i2)) (* (nth b i1) (nth a i2))))
+(defn det2 [a b i1 i2]
+  {:pre [(= (count a) (count b))]
+   :post [true]}
+  (- (* (nth a i1) (nth b i2)) (* (nth b i1) (nth a i2))))
 (defn vect [& args] (reduce #(vector (det2 %1 %2 1 2) (- (det2 %1 %2 0 2)) (det2 %1 %2 0 1)) args))
+; matrix
+(def m+ (doOp v+))
+(def m- (doOp v-))
+(def m* (doOp v*))
+(def md (doOp vd))
+(defn m*s [mat & args] (mapv #(v*s %1 (apply * args)) mat))
+(defn m*v [mat vec] (mapv (partial scalar vec) mat))
+(defn transpose [mat] (apply mapv vector mat))
+(defn m*m [& args] (reduce #(transpose ((fn [x y] (mapv (partial m*v x) (transpose y))) %1 %2)) args))
+; cuboid
+(def c+ (doOp m+))
+(def c- (doOp m-))
+(def c* (doOp m*))
+(def cd (doOp md))
+; tensor
+(defn tensorOp [f & args]
+  {:pre [(apply allVectors args) (apply sameLength args)]}
+  (if (vector? (first (first args))) (apply mapv (partial tensorOp f) args) (apply (doOp f) args)))
+(def t+ (partial tensorOp +))
+(def t- (partial tensorOp -))
+(def t* (partial tensorOp *))
