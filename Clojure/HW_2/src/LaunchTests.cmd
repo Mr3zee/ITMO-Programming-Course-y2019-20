6de490facf20801e6aa624353d1db75e04802bfa
Mr3zee <284627@niuitmo.ru>
2020-05-01 03:00:31 +0300

    added pw/lg

--- Clojure/HW_2/src/LaunchTests.cmd
+++ Clojure/HW_2/src/LaunchTests.cmd
@@ -1,1 +1,1 @@
-C:\Users\809542\Desktop\korneev\paradigms-2020\clojure\TestClojure.cmd cljtest.functional.ClojureFunctionalMedAvgTest hard
+C:\Users\809542\Desktop\korneev\paradigms-2020\clojure\TestClojure.cmd cljtest.functional.ClojureFunctionalPwLgTest hard
--- Clojure/HW_2/src/expression.clj
+++ Clojure/HW_2/src/expression.clj
@@ -7,9 +7,11 @@
 
 ; math
 (defn div [& args] (reduce #(/ (double %1) %2) args))
-(def math-ln #(div (Math/log (Math/abs %1)) (Math/log Math/E)))
-(defn math-avg [& args] (let [c (count args)] (div (apply + args) c)))
+(def math-ln #(Math/log (Math/abs %1)))
+(def math-avg #(let [c (count %&)] (div (apply + %&) c)))
 (def math-med #(let [k (div (count %&) 2)] (nth (sort %&) k)))
+(def math-pow #(Math/pow %1 %2))
+(def math-log #(div (math-ln %2) (math-ln %1)))
 
 ; operations
 (defn basic-op [f & args] (fn [vars] (apply f ((apply juxt args) vars))))
@@ -24,11 +26,13 @@
 (def ln (partial basic-op math-ln))
 (def avg (partial basic-op math-avg))
 (def med (partial basic-op math-med))
+(def pw (partial basic-op math-pow))
+(def lg (partial basic-op math-log))
 ; todo hw_8 js name class Error
 
 ; parse
 (def lexemes {"+" add "-" subtract "*" multiply "/" divide "negate" negate
-              "min" min "max" max "exp" exp "ln" ln "avg" avg "med" med})
+              "min" min "max" max "exp" exp "ln" ln "avg" avg "med" med "pw" pw "lg" lg})
 (def vars {"x" x "y" y "z" z})
 (defn extract [arg]
   (cond
