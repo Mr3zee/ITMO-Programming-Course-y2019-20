45ecd4ac54d8254b714749d11c328f626757d52f
Mr3zee <284627@niuitmo.ru>
2020-05-15 12:44:23 +0300

    Object added

--- Clojure/HW_2/src/expression.clj
+++ Clojure/HW_2/src/expression.clj
@@ -1,3 +1,5 @@
+(require '[clojure.string :as string])
+
 ; constants and vars
 (defn constant [val] (fn [vars] val))
 (defn variable [name] (fn [vars] (get vars name)))
@@ -48,18 +50,116 @@
               "pw" pw
               "lg" lg
               })
-(def vars {
+(def vars-function {
            "x" x
            "y" y
            "z" z
            })
-(defn extract [arg]
+
+(defn parse [lexs vars const]
+  (letfn [
+          (extract [arg]
+                  (cond
+                    (list? arg) (let [
+                                      [f & args] arg
+                                      f' (get lexs (name f))
+                                      ]
+                                  (apply f' (map extract args)))
+                    (symbol? arg) (get vars (name arg))
+                    :else (const arg)))
+        ] (fn [expression] (extract (read-string expression)))))
+(def parseFunction (parse lexemes vars-function constant))
+; Objects
+
+(defn proto-get [obj key]
   (cond
-    (list? arg) (let [
-                      [f & args] arg
-                      f' (get lexemes (name f))
-                      ]
-                  (apply f' (map extract args)))
-    (symbol? arg) (get vars (name arg))
-    :else (constant arg)))
-(defn parseFunction [expression] (extract (read-string expression)))
+    (contains? obj key) (obj key)
+    (contains? obj :proto) (proto-get (obj :proto) key)))
+(defn proto-call [obj key & args] (apply (proto-get obj key) obj args))
+(defn field [key] #(proto-get % key))
+(defn method [key] #(apply proto-call %1 key %&))
+(defn constructor [obj proto] #(apply obj {:proto proto} %&))
+
+(def _name (field :name))
+(def _value (field :value))
+(def _args (field :args))
+(def _op (field :op))
+(def diff-rule (field :diff-rule))
+(def evaluate (method :evaluate))
+(def toString (method :toString))
+(def diff (method :diff))
+
+(def Constant)
+(def const-proto
+  {
+   :evaluate  (fn [this _] (_value this))
+   :toString  #(format "%.1f" (_value %))
+   :diff      (fn [_ _] (Constant 0))
+   })
+(defn const-constructor [this val] (assoc this :value val))
+(def Constant (constructor const-constructor const-proto))
+(def ZERO (Constant 0))
+(def ONE (Constant 1))
+
+(def variable-proto
+  {
+   :evaluate  #(get %2 (_name %1))
+   :toString  #(_name %)
+   :diff      #(if (= (_name %1) %2) ONE ZERO)
+   })
+(defn variable-constructor [this name] (assoc this :name name))
+(def Variable (constructor variable-constructor variable-proto))
+(def X (Variable "x"))
+(def Y (Variable "y"))
+(def Z (Variable "z"))
+
+(def operation-proto
+  {
+   :diff      (fn [this var] ((diff-rule this) (_args this) var))
+   :evaluate  (fn [this vars] (apply (_op this) (mapv #(evaluate % vars) (_args this))))
+   :toString  #(str "(" (_name %) " " (string/join " " (mapv toString (_args %))) ")")
+   })
+(defn operation-constructor [this operation name rule]
+  (fn [& args] (assoc this :op operation :args args :name name :diff-rule rule)))
+(defn to-diff [args var] (mapv #(diff % var) args))
+
+(def Basic (constructor operation-constructor operation-proto))
+(def Add (Basic + "+" (fn [args var] (apply Add (to-diff args var)))))
+(def Sum (Basic + "sum" (fn [args var] (apply Sum (to-diff args var)))))
+(def Avg (Basic math-avg "avg" (fn [args var] (apply Avg (to-diff args var)))))
+(def Subtract (Basic - "-" (fn [args var] (apply Subtract (to-diff args var)))))
+(def Negate (Basic - "negate" (fn [args var] (apply Subtract (to-diff args var)))))
+(def Multiply (Basic * "*"
+                     (fn [args var] (reduce
+                                      (fn [a b]
+                                        (Add
+                                          (Multiply (diff a var) b)
+                                          (Multiply (diff b var) a)
+                                          )) args))))
+(def Divide (Basic div "/"
+                   (fn [args var] (reduce
+                                     (fn [a b]
+                                       (Divide
+                                         (Subtract
+                                           (Multiply (diff a var) b)
+                                           (Multiply (diff b var) a)
+                                           )
+                                         (Multiply b b)
+                                         )) args))))
+
+(def object-lexemes
+  {
+   "+" Add
+   "-" Subtract
+   "*" Multiply
+   "/" Divide
+   "negate" Negate
+   "avg" Avg
+   "sum" Sum
+   })
+(def vars-object {
+                    "x" X
+                    "y" Y
+                    "z" Z
+                    })
+(def parseObject (parse object-lexemes vars-object Constant))
--- Clojure/HW_2/src/test.cmd
+++ Clojure/HW_2/src/test.cmd
@@ -1,1 +1,2 @@
 powershell -command clj expression.clj
+C:\Users\809542\Desktop\korneev\paradigms-2020\clojure\TestClojure.cmd cljtest.object.ClojureObjectSumAvgTest hard
