3cc6a0be2b7371973ad3dcd729a31c00810ee936
Mr3zee <284627@niuitmo.ru>
2020-09-03 23:38:47 +0300

    Java README files added HW_1-9

--- Java/HW_2/HexTest.java
+++
@@ -1,6 +0,0 @@
-public class HexTest {
-	public static void main(String[] args) {
-		int n = Integer.parseInt(args[0]);
-		System.out.println(n);
-	}
-}
--- Java/HW_2/test.java
+++
@@ -1,6 +0,0 @@
-public class test{
-	public static void main(String[] args){
-		int n = Character.isWhitespace('');
-		System.out.print(n);
-	}
-}
